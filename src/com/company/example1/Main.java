ba47cd331040f8249d507c787525c7823f2aac9c
kubawieczorek <jwieczorekdevelop@gmail.com>
2019-04-13 16:27:06 +0200

    Revert "current stage of project"
    This reverts commit f3acdf7cb26263727db8a86e82d390a4fcae6374.

--- src/com/company/example1/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company.example1;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(NIOFileLoader.countYellow("C:\\Users\\B51-80\\Documents\\javaNioTestApp\\test.txt"));
-    }
-}
